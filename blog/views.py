8776991f15ceb9adcafa4df28f4412d57e79722c
Oznur Celebi <o.ozgumus17@gmail.com>
2022-02-11 21:15:41 +0100

    PostListView and PostDetailView created

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView,
+)
 from .models import Post
 
 def home(request):
@@ -7,6 +13,14 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
+class PostListView(ListView):
+    model =Post
+    template_name ='blog/home.html' #<app>/<model>_<viewtype>.html
+    context_object_name ='posts'
+    ordering =['-date_posted']
+
+class PostDetailView(DetailView):
+    model =Post
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
