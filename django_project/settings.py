555c50a2d5072dcf6940fb89c932abcce2b62583
Oznur Celebi <o.ozgumus17@gmail.com>
2022-02-16 20:19:28 +0100

    static-root parent version

--- django_project/settings.py
+++ django_project/settings.py
@@ -127,8 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-#STATIC_ROOT = BASE_DIR /'staticfiles'
-STATIC_ROOT = "/var/www/django_project.com/static/"
+STATIC_ROOT = (BASE_DIR /'staticfiles')
+#STATIC_ROOT = "/var/www/django_project.com/static/"
 STATIC_URL = '/static/'
 STATICFILES_DIRS = BASE_DIR /'static'
 STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
